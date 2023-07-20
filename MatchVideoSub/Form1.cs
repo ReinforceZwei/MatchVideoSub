fc4e095b2c7bad1acaa0c216191f9f58fa15ca6c
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2023-07-20 20:37:18 +0800

    Update to use nature sorting

--- MatchVideoSub/Form1.cs
+++ MatchVideoSub/Form1.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace MatchVideoSub
 {
@@ -13,6 +14,7 @@ namespace MatchVideoSub
         private void videoDropPanel_DragDrop(object sender, DragEventArgs e)
         {
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            List<string> toAdd = new();
             foreach (string file in files)
             {
                 var fileAttr = File.GetAttributes(file);
@@ -23,8 +25,7 @@ namespace MatchVideoSub
                     {
                         if (!MatchSub.FileShouldExclude(Path.GetFileName(dirFile)))
                         {
-                            videoListBox.Items.Add(dirFile);
-                            videoListBox.SetItemChecked(videoListBox.Items.Count - 1, true);
+                            toAdd.Add(dirFile);
                         }
                     }
                 }
@@ -32,17 +33,30 @@ namespace MatchVideoSub
                 {
                     if (!MatchSub.FileShouldExclude(Path.GetFileName(file)))
                     {
-                        videoListBox.Items.Add(file);
-                        videoListBox.SetItemChecked(videoListBox.Items.Count - 1, true);
+                        toAdd.Add(file);
                     }
                 }
                 Debug.WriteLine(file);
+            }
+            // Combine existing items
+            toAdd.AddRange(videoListBox.Items.Cast<string>().ToList());
+            videoListBox.Items.Clear();
+
+            // Nature sorting the subtitles
+            toAdd.OrderBy(x => x, new NaturalStringComparer())
+                .ToList()
+                .ForEach(x => videoListBox.Items.Add(x));
+
+            for (int i = 0; i < videoListBox.Items.Count; i++)
+            {
+                videoListBox.SetItemChecked(i, true);
             }
         }
 
         private void subDropPanel_DragDrop(object sender, DragEventArgs e)
         {
             string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            List<string> toAdd = new();
             foreach (string file in files)
             {
                 var fileAttr = File.GetAttributes(file);
@@ -53,8 +67,8 @@ namespace MatchVideoSub
                     {
                         if (!MatchSub.FileShouldExclude(Path.GetFileName(dirFile)))
                         {
-                            subListBox.Items.Add(dirFile);
-                            subListBox.SetItemChecked(subListBox.Items.Count - 1, true);
+                            toAdd.Add(dirFile);
+                            //subListBox.SetItemChecked(subListBox.Items.Count - 1, true);
                         }
                     }
                 }
@@ -62,11 +76,24 @@ namespace MatchVideoSub
                 {
                     if (!MatchSub.FileShouldExclude(Path.GetFileName(file)))
                     {
-                        subListBox.Items.Add(file);
-                        subListBox.SetItemChecked(subListBox.Items.Count - 1, true);
+                        toAdd.Add(file);
+                        //subListBox.SetItemChecked(subListBox.Items.Count - 1, true);
                     }
                 }
                 Debug.WriteLine(file);
+            }
+            // Combine existing items
+            toAdd.AddRange(subListBox.Items.Cast<string>().ToList());
+            subListBox.Items.Clear();
+
+            // Nature sorting the subtitles
+            toAdd.OrderBy(x => x, new NaturalStringComparer())
+                .ToList()
+                .ForEach(x => subListBox.Items.Add(x));
+
+            for (int i = 0; i < subListBox.Items.Count; i++)
+            {
+                subListBox.SetItemChecked(i, true);
             }
         }
 
@@ -98,45 +125,49 @@ namespace MatchVideoSub
 
         private void renameBtn_Click(object sender, EventArgs e)
         {
-            List<string> videoNames = new List<string>();
-            List<string> subNames = new List<string>();
-            List<string> subPath = subListBox.CheckedItems.Cast<string>().OrderBy(x => x).ToList();
-            videoNames = videoListBox.CheckedItems.Cast<string>().Select(x => Path.GetFileName(x)).OrderBy(x => x).ToList();
-            subNames = subListBox.CheckedItems.Cast<string>().Select(x => Path.GetFileName(x)).OrderBy(x => x).ToList();
+            List<string> videoPaths = videoListBox.CheckedItems.Cast<string>().ToList();
+            List<string> subPaths = subListBox.CheckedItems.Cast<string>().ToList();
 
-            if (videoNames.Count != subNames.Count)
-            {
-                MessageBox.Show("Number of videos and subtitles does not match", "Warning");
-                return;
-            }
-
-            if (videoNames.Count == 0 || subNames.Count == 0)
+            if (videoPaths.Count == 0 || subPaths.Count == 0)
             {
                 return;
             }
-
-            var result = MatchSub.RenameSubtitles(videoNames, subNames);
-            var subDestPath = Path.GetDirectoryName(subPath[0]);
 
             if (saveCopyCheckBox.Checked)
             {
                 var dlg = new FolderPicker();
                 if (dlg.ShowDialog(Handle) == true)
                 {
-                    subDestPath = dlg.ResultPath;
+                    try
+                    {
+                        // Save a new copy
+                        MatchSub.Match(videoPaths, subPaths, dlg.ResultPath);
+                        MessageBox.Show("Subtitle saved.", "OK");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Number of videos and subtitles does not match", "Warning");
+                        return;
+                    }
                 }
                 else return;
             }
-
-            for (var i = 0; i < subPath.Count; i++)
+            else
             {
-                if (saveCopyCheckBox.Checked)
-                    File.Copy(subPath[i], Path.Combine(subDestPath, result[i]), true);
-                else
-                    File.Move(subPath[i], Path.Combine(subDestPath, result[i]), true);
+                try
+                {
+                    // Rename subtitles
+                    MatchSub.Match(videoPaths, subPaths);
+                    MessageBox.Show("Subtitle renamed.", "OK");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Number of videos and subtitles does not match", "Warning");
+                    return;
+                }
             }
 
-            MessageBox.Show("Subtitle renamed.", "OK");
+            
         }
     }
 }
--- MatchVideoSub/MatchSub.cs
+++ MatchVideoSub/MatchSub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,12 +37,19 @@ namespace MatchVideoSub
             return false;
         }
 
+        private static bool IsValidList(IList<string> videoPaths, IList<string> subPaths)
+        {
+            return videoPaths.Count == subPaths.Count 
+                && videoPaths.Count > 0 
+                && subPaths.Count > 0;
+        }
+
         public static bool FileShouldExclude(string filename)
         {
             return WildcardMatch(filename);
         }
 
-        public static IList<string> RenameSubtitles(IList<string> videoNames, IList<string> subNames)
+        public static IList<string> CreateSubtitleFilename(IList<string> videoNames, IList<string> subNames)
         {
             if (videoNames.Count != subNames.Count)
                 throw new Exception("Number of videos and subtitles does not match");
@@ -53,6 +61,53 @@ namespace MatchVideoSub
                 newSub.Add(Path.GetFileNameWithoutExtension(videoName) + subExt);
             }
             return newSub;
+        }
+
+        public static void Match(IList<string> videoPaths, IList<string> subPaths)
+        {
+            if (!IsValidList(videoPaths, subPaths))
+                throw new Exception("Number of video and subtitle mismatch");
+
+            videoPaths = videoPaths.OrderBy(x => x, new NaturalStringComparer()).ToList();
+            subPaths = subPaths.OrderBy(x => x, new NaturalStringComparer()).ToList();
+
+            var destPath = Path.GetDirectoryName(subPaths[0]);
+            var newSubtitle = CreateSubtitleFilename(videoPaths, subPaths);
+
+            for (int i = 0; i < videoPaths.Count; i++)
+            {
+                File.Move(subPaths[i], Path.Combine(destPath, newSubtitle[i]), true);
+            }
+        }
+
+        public static void Match(IList<string> videoPaths, IList<string> subPaths, string destPath)
+        {
+            if (!IsValidList(videoPaths, subPaths))
+                throw new Exception("Number of video and subtitle mismatch");
+
+            videoPaths = videoPaths.OrderBy(x => x, new NaturalStringComparer()).ToList();
+            subPaths = subPaths.OrderBy(x => x, new NaturalStringComparer()).ToList();
+
+            var newSubtitle = CreateSubtitleFilename(videoPaths, subPaths);
+
+            for (int i = 0; i < videoPaths.Count; i++)
+            {
+                File.Copy(subPaths[i], Path.Combine(destPath, newSubtitle[i]), true);
+            }
+        }
+    }
+
+    internal static class SafeNativeMethods
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+
+    public sealed class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a, b);
         }
     }
 }
